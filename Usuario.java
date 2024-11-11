a1a21ec4c9a4a7503449f53d45acb10440ef9cab
clcumbe <clcumbe@espol.edu.ec>
2024-11-10 22:16:14 -0500

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,93 @@
+public abstract class Usuario {
+    private String codigoUnico;
+    private String cedula;
+    private String nombres;
+    private String apellidos;
+    private String usuario;
+    private String contrasena;
+    private String correo;
+    private String rol;
+
+    public Usuario(String codigoUnico, String cedula, String nombres, String apellidos, 
+                   String usuario, String contrasena, String correo, String rol) {
+        this.codigoUnico = codigoUnico;
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.correo = correo;
+        this.rol = rol;
+    }
+
+    public String getCodigoUnico() {
+        return codigoUnico;
+    }
+
+    public void setCodigoUnico(String codigoUnico) {
+        this.codigoUnico = codigoUnico;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+ 
+    public abstract void realizarReserva();
+
+    public void enviarNotificacion() {
+        System.out.println("Notificación enviada a: " + correo);
+    }
+}
