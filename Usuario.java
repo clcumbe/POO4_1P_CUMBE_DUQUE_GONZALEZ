d2b3b5a9a59e274ea9f0b554bdfeea2f390b9cec
DanielDuque357 <dduquecastro@gmail.com>
2024-11-10 22:42:57 -0500

    Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -90,4 +90,55 @@ public abstract class Usuario {
     public void enviarNotificacion() {
         System.out.println("Notificación enviada a: " + correo);
     }
+    public class Espacio {
+   private String codigoUnico;
+   private String tipoEspacio;
+   private String nombre;
+   private int capacidad;
+   private String estado;
+   private String rolPermitido;
+
+   public Espacio(String codigoUnico, String tipoEspacio, String nombre, int capacidad, String estado, String rolPermitido) {
+       this.codigoUnico = codigoUnico;
+       this.tipoEspacio = tipoEspacio;
+       this.nombre = nombre;
+       this.capacidad = capacidad;
+       this.estado = estado;
+       this.rolPermitido = rolPermitido;
+   }
+
+   public String getCodigoUnico() {
+       return codigoUnico;
+   }
+
+   public String getTipoEspacio() {
+       return tipoEspacio;
+   }
+
+   public String getNombre() {
+       return nombre;
+   }
+
+   public int getCapacidad() {
+       return capacidad;
+   }
+
+   public String getEstado() {
+       return estado;
+   }
+
+   public String getRolPermitido() {
+       return rolPermitido;
+   }
+
+   public void setEstado(String estado) {
+       this.estado = estado;
+   }
+
+   public boolean esReservablePor(String rol) {
+       return rolPermitido.equals("AMBOS") || rolPermitido.equals(rol);
+   }
 }
+
+}
+
